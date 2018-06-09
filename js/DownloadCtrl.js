dcd98a86c1976ea3bc7e1f3c1035bf71bd761841
Mathias Quintero <me@quintero.io>
2018-06-09 17:40:41 +0200

    Update DownloadCtrl.js

--- js/DownloadCtrl.js
+++ js/DownloadCtrl.js
@@ -2,7 +2,7 @@ app.controller('DownloadCtrl', function($scope, $routeParams, $http) {
 
     $http.get("https://backend.break-out.org/posting/" + $routeParams.id + "/").then(function(res) {
         var media = res.data.media;
-        if (media.length > 0) {
+        if (media) {
             var url = media[0].url;
             var a = document.createElement('a');
             a.download = url; // Set the file name.
